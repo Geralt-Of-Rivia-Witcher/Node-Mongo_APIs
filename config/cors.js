e0ec6d0ed0ae001942790ed37e413490240718d1
Geralt of Rivia <singhsiddhantkumar@gmail.com>
2022-03-27 20:09:16 +0530

    Deleted unused file

--- config/cors.js
+++
@@ -1,5 +0,0 @@
-export const corsOptions = {
-    // origin: ["http://localhost:3000"],
-    // credentials: true,
-    // exposedHeaders: ["set-cookie"],
-};
--- index.js
+++ index.js
@@ -2,13 +2,12 @@ import express from "express";
 import cors from "cors";
 
 import { PORT } from "./config/config.js";
-import { corsOptions } from "./config/cors.js";
 import createDbConnection from "./loaders/mongo.js";
 import routes from "./routes/routes.js";
 
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
